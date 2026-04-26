3ca6adfad5a1c42d142e47c4f60d62e9853c1fca
Moises Núñez Ibarra <mizulegendsgg@gmail.com>
2026-04-26 08:54:32 -0500

    Create home.js

---
+++ nodes/home.js
@@ -0,0 +1,5 @@
+// nodes/home.js
+MizuEngine.db['🏠 INICIO'] = {
+    items: ['🏢 Mizu Enterprise Suite', '🔬 Mizu Research Nexus', '🎨 Mizu Creative Workshop'],
+    desc: 'Navega por los pilares fundamentales de Mizu Legends Studios.'
+};
